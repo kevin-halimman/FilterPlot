b1d039aa699039d7732e768f59046764ee3a50b5
Kevin Halimman <modderkevinh@gmail.com>
2026-01-09 08:50:34 -0500

    Added RC to CR inversion option

--- app.py
+++ app.py
@@ -11,13 +11,21 @@ root.title("Filter Transfer Function Visualization")
 root.geometry("1200x700")
 canvas = None
 j = 1j
+RC_invert = False
+
 
 def clear_frame():
     for widget in root.winfo_children():
         widget.destroy()
 
+def toggle_RC_invert():
+    global RC_invert
+    RC_invert = not RC_invert
+    #RC_Circuit()
+
 def RC_Circuit():
     clear_frame()
+    global RC_invert
 
     resistance_input = tk.Label(root, text="Enter Resistance (R) in Ohms:")
     resistance_input.pack(pady=5)
@@ -28,19 +36,26 @@ def RC_Circuit():
     entry2 = tk.Entry(root)
     entry2.pack(pady=5)
     ttk.Button(root, text="Plot RC Circuit", command=lambda: plot_RC(entry1, entry2)).pack(pady=20)
+    label_info = tk.Label(root, text="Switch from RC -> CR and vice versa:").pack(pady=5)
+    ttk.Button(root, text="Toggle Invert/Non-Invert", command=lambda: toggle_RC_invert()).pack(pady=5)
 
 def plot_RC(entry1, entry2):
     if not entry1.get() or not entry2.get():
         return
     
     global canvas
+    global RC_invert
     R = float(entry1.get())
     C = float(entry2.get())
 
     wc = 1/(R*C)
     fc = wc/(2*np.pi)
     w = np.logspace(1, 6, num=500)
-    H = (1/(j*w*C))/(R + 1/(j*w*C))
+
+    if RC_invert:
+        H = R/(R + 1/(j*w*C))
+    else:
+        H = (1/(j*w*C))/(R + 1/(j*w*C))
 
     if canvas is not None:
         canvas.get_tk_widget().destroy()
@@ -67,6 +82,6 @@ def plot_RC(entry1, entry2):
     canvas.draw()
     canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
-
 ttk.Button(root, text = "RC Circuit", command = RC_Circuit).pack()
+ttk.Button(root, text = "Exit", command = root.destroy).pack(pady=10)
 root.mainloop()
